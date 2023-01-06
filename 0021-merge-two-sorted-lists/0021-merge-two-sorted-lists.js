6adfccaa57bde05e72ba0fc7c76b284875c770d8
Kobe <68458245+sjh50200@users.noreply.github.com>
2023-01-06 12:51:39 +0900

    Time: 97 ms (56.15%), Space: 44.3 MB (48.55%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -13,15 +13,13 @@
 var mergeTwoLists = function(list1, list2) {
     let result;
     if (!list1) return list2;
-    else if (!list2) return list1;
-    else {
-        if(list1.val <= list2.val) {
-            result = list1;
-            result.next = mergeTwoLists(list1.next, list2)
-        } else {
-            result = list2;
-            result.next = mergeTwoLists(list1, list2.next);
-        }
+    if (!list2) return list1;
+    if(list1.val <= list2.val) {
+        result = list1;
+        result.next = mergeTwoLists(list1.next, list2)
+    } else {
+        result = list2;
+        result.next = mergeTwoLists(list1, list2.next);
     }
     return result;
 };
